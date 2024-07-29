21f5d27e205b1b6be96ae34896bf66604d6340d0
Puneetbisarwal <puneetbisarwal@gmail.com>
2024-07-30 00:26:35 +0530

    My ninth commit Video Part 18 Angular pipes

--- src/app/employee/employeeList.component.ts
+++ src/app/employee/employeeList.component.ts
@@ -11,10 +11,10 @@ export class EmployeeListComponent {
 
    constructor() { 
     this.employees = [
-      { code: 'Emp101', name: 'Akash', gender: 'Male', annualSalary: 5500, dateOfBirth: '25/06/1994' },
-      { code: 'Emp102', name: 'Vishal', gender: 'Male', annualSalary: 6900.95, dateOfBirth: '18/11/1995' },
-      { code: 'Emp103', name: 'Medha', gender: 'Female', annualSalary: 7500, dateOfBirth: '12/04/1994' },
-      { code: 'Emp104', name: 'Nimish', gender: 'Male', annualSalary: 6700.725, dateOfBirth: '20/01/1994' },
+      { code: 'Emp101', name: 'Akash', gender: 'Male', annualSalary: 5500, dateOfBirth: '06/25/1994' },
+      { code: 'Emp102', name: 'Vishal', gender: 'Male', annualSalary: 6900.95, dateOfBirth: '11/18/1995' },
+      { code: 'Emp103', name: 'Medha', gender: 'Female', annualSalary: 7500, dateOfBirth: '04/12/1994' },
+      { code: 'Emp104', name: 'Nimish', gender: 'Male', annualSalary: 6700.725, dateOfBirth: '01/20/1994' },
       
      ];
    
@@ -22,11 +22,11 @@ export class EmployeeListComponent {
 
   getEmployees(): void {
     this.employees = [
-      { code: 'Emp101', name: 'Akash', gender: 'Male', annualSalary: 5500, dateOfBirth: '25/06/1994' },
-      { code: 'Emp102', name: 'Vishal', gender: 'Male', annualSalary: 6900.95, dateOfBirth: '18/11/1995' },
-      { code: 'Emp103', name: 'Medha', gender: 'Female', annualSalary: 7500, dateOfBirth: '12/04/1994' },
-      { code: 'Emp104', name: 'Nimish', gender: 'Male', annualSalary: 6700.725, dateOfBirth: '20/01/1994' },
-      { code: 'Emp105', name: 'Akshita', gender: 'Female', annualSalary: 6600.225, dateOfBirth: '12/01/1995' },
+      { code: 'Emp101', name: 'Akash', gender: 'Male', annualSalary: 5500, dateOfBirth: '06/25/1994' },
+      { code: 'Emp102', name: 'Vishal', gender: 'Male', annualSalary: 6900.95, dateOfBirth: '11/18/1995' },
+      { code: 'Emp103', name: 'Medha', gender: 'Female', annualSalary: 7500, dateOfBirth: '04/12/1994' },
+      { code: 'Emp104', name: 'Nimish', gender: 'Male', annualSalary: 6700.725, dateOfBirth: '01/20/1994' },
+      { code: 'Emp105', name: 'Akshita', gender: 'Female', annualSalary: 6600.225, dateOfBirth: '01/12/1995' },
       
     ];
   }
