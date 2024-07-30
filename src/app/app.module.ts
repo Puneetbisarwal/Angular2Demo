88d348e6ab92947cb26454ee7aa98ebe2566a30a
Puneetbisarwal <puneetbisarwal@gmail.com>
2024-07-30 16:02:18 +0530

    My tenth commit Video Part 19 Angular cuustom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { EmployeeListComponent } from './employee/employeeList.component';
+import { EmployeeTitlePipe } from './employee/employeeTitle.pipe';
 
 @NgModule({
-  declarations: [AppComponent, EmployeeComponent, EmployeeListComponent],
+  declarations: [AppComponent, EmployeeComponent, EmployeeListComponent, EmployeeTitlePipe],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
