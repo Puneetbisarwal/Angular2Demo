e778a77ee014b8717dc8aba6fba78284bda79590
Puneetbisarwal <puneetbisarwal@gmail.com>
2024-07-31 22:52:26 +0530

    My 16th commit Video Part 25 Angular services tutorial

--- src/app/employee/employee.service.ts
+++ src/app/employee/employee.service.ts
@@ -6,7 +6,7 @@ export class EmployeeService {
     getEmployees(): IEmployee[] {
         return [
             { code: 'emp101', name: 'Akash', gender: 'Male', annualSalary: 5500, dateOfBirth: '06/25/1994' },
-            { code: 'emp102', name: 'Vishal', gender: 'Male', annualSalary: 6900.95, dateOfBirth: '11/18/1995' },
+            { code: 'emp102', name: 'Vishal', gender: 'Male', annualSalary: 6900.925, dateOfBirth: '11/18/1995' },
             { code: 'emp103', name: 'Medha', gender: 'Female', annualSalary: 7500, dateOfBirth: '04/12/1994' },
             { code: 'emp104', name: 'Nimish', gender: 'Male', annualSalary: 6700.725, dateOfBirth: '01/20/1994' },
             { code: 'emp105', name: 'Akshita', gender: 'Female', annualSalary: 6600.225, dateOfBirth: '01/12/1995' },
