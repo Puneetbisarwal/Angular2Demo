ca52f263e1bc6123503d9fe46e567cb95b57ab3f
Puneetbisarwal <puneetbisarwal@gmail.com>
2024-07-31 18:46:44 +0530

    My 14th commit Video Part 23 Interfaces in Angular 2

--- src/app/employee/employeeList.component.ts
+++ src/app/employee/employeeList.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IEmployee } from './employee';
 
 @Component({
   selector: 'list-employee',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class EmployeeListComponent {
 
-  employees: any[];
+  employees: IEmployee[];
 
 
   selectedEmployeeCountRadioButtonButton: string = 'All';
